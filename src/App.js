9a01b623c1227ff2f44d6b5245a5a3ec312d34c2
sofiacottone <cottonesofia@outlook.it>
2024-07-30 12:09:50 +0200

    feat: add footer
    - add Stats component in footer to display list statistics
    - import component in App.js
    - add style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import AddItems from "./components/AddItems";
 import ItemsList from "./components/ItemsList";
+import Stats from "./components/Stats";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <main>
         <AddItems />
         <ItemsList />
+        <Stats />
       </main>
     </div>
   );
---
+++ src/components/Stats.js
@@ -0,0 +1,9 @@
+export default function Stats() {
+    return (
+        <footer className="stats">
+            <em>
+                You have n items on your list, and you already packed n (n%)
+            </em>
+        </footer>
+    );
+}
